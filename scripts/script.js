8a30a380cf2fb2e571ecc563ecfb4bf810d1a044
Sundar Krishnamurthy <sundar_k@hotmail.com>
2020-04-18 14:39:15 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,127 +1,180 @@
+// JavaScript source code// JavaScript source code
 var letters = "SLLEWOTNSLETEHOOTAKECRIBCBDAKLUQUAYEBARCPALADNASB";
-var words = ["SNORKEL","TOWEL","BUCKET","KITE","TABLE","SPADE","SANDAL","SAND","QUAY","BAY","YACHT"];
+var words = ["TOWEL", "SANDAL", "BAY", "YACHT", "BUCKET", "KITE", "SPADE", "BALL", "CUP", "SNORKEL", "QUAY", "SAND", "CUP", "SOIL", "TEA", "CRAB", "TABLE", "ROLE"];
 var rowLength = 7;
 var columnLength = rowLength;
 var mazeCount = letters.length;
-var coordinates = new Array();
+var colorIndices = new Array();
 var resetList = new Array();
+var insideIndex = -1;
 var canvasLocation = null;
+var mousePressed = false;
+var mouseMotion = false;
+var moveChanged = false;
+var mouseX = 0;
+var mouseY = 0;
+var xLimit = 0;
+var yLimit = 0;
+var defaultColor = "black";
+var highlightColor = "green";
+var defaultFont = "24pt Courier";
+var highlightFont = "bold 24pt Courier";
 
 function renderCanvas() {
     checkCanvasForConsistency();
-
+ 
     var canvas = document.getElementById("mazeCanvas");
     canvas.addEventListener("mousemove", doMouseMove, false);
-
+    canvas.addEventListener("mousedown", doMouseDown, false);
+    canvas.addEventListener("mouseup", doMouseUp, false);
+ 
     canvas.addEventListener("touchstart", doTouchStart, false);
     canvas.addEventListener("touchmove", doTouchMove, false);
     canvas.addEventListener("touchend", doTouchEnd, false);
-
+ 
     var context = canvas.getContext("2d");
-    context.font = "bold 24pt Courier";
-    context.fillStyle = "black";
-
-    canvasLocation = canvas.getBoundingClientRect();
-
-    var x = 30;
+    context.font = defaultFont;
+    context.fillStyle = defaultColor;
+ 
+    var x = 40;
     var y = 40;
     var columnCounter = 0;
-
+ 
     for (var i = 0; i < mazeCount; i++) {
         if (columnCounter == columnLength) {
-            x = 30;
+ 
+            if (xLimit == 0) {
+                xLimit = x - 19;
+            }
+ 
+            x = 40;
             y += 40;
             columnCounter = 0;
         }
-
+ 
         var c = letters.charAt(i);
+        console.log("Filling " + letters.charAt(i) + " at " + x + " " + y);
         context.fillText(c, x, y);
-
-        //        context.strokeRect(x, y - 20, 20, 20);
-        coordinates.push(new Array(x, y - 20, x + 20, y));
-
-        x += 30;
+        colorIndices.push(defaultColor);
+ 
+        x += 40;
         columnCounter++;
+ 
+        if (i == mazeCount - 1) {
+            yLimit = y + 5;
+        }
     }
 }
-
+ 
+function doMouseDown(event) {
+    if (!mousePressed) {
+        mousePressed = true;
+        mouseX = event.x;
+        mouseY = event.y;
+        console.log("mouse down");
+    }
+}
+ 
+function doMouseUp(event) {
+    mousePressed = false;
+    mouseMotion = false;
+    console.log("mouse up");
+}
+ 
 function doMouseMove(event) {
     var canvas = document.getElementById("mazeCanvas");
-
+ 
     canvasLocation = canvas.getBoundingClientRect();
-
-    canvas_x = event.x;
-    canvas_y = event.y;
-    var txtCoord = document.getElementById("txtCoord");
-    txtCoord.value = canvas_x + " " + canvas_y;
-
-    var insideSquare = false;
-
-    for (i = 0; i < mazeCount; i++) {
-        var coordinate = coordinates[i];
-
-        if ((canvas_x > coordinate[0] + canvasLocation.left) &&
-            (canvas_y >= coordinate[1] + canvasLocation.top) &&
-            (canvas_x < coordinate[2] + canvasLocation.left) &&
-            (canvas_y <= coordinate[3] + canvasLocation.top)) {
-            var c = letters.charAt(i);
+ 
+    canvas_x = event.x - canvasLocation.left;
+    canvas_y = event.y - canvasLocation.top;
+ 
+    if ((canvas_x < 35) || (canvas_x > xLimit) || (canvas_y > yLimit)) {
+ 
+        if (insideIndex != -1) {
+ 
+            x_offset = (parseInt(insideIndex % columnLength) + 1) * 40;
+            y_offset = (parseInt(insideIndex / columnLength) + 1) * 40;
+ 
             var context = canvas.getContext("2d");
-
-            var context = canvas.getContext("2d");
-            context.font = "bold 24pt Courier";
             context.fillStyle = "white";
-            context.fillText(c, coordinate[0], coordinate[3]);
+            context.fillRect(x_offset, y_offset - 20, 20, 20);
 
-            context.font = "bold 24pt Courier";
-
-            context.fillStyle = "red";
-
-            context.fillText(c, coordinate[0], coordinate[3]);
-            insideSquare = true;
-
-            resetList.push(new Array(i, coordinate[0], coordinate[3]));
-
-            break;
+            context.font = defaultFont;
+            context.fillStyle = colorIndices[insideIndex];
+ 
+            console.log("Filling " + letters.charAt(insideIndex) + " at " + x_offset + " " + y_offset);
+            context.fillText(letters.charAt(insideIndex), x_offset, y_offset);
+ 
+            insideIndex = -1;
         }
+ 
+        return;
     }
-
-    if ((insideSquare == false) &&
-        (resetList.length > 0)) {
-        for (i = 0; i < resetList.length; i++) {
-
-            var element = resetList[i];
-
-            var c = letters.charAt(element[0]);
+ 
+    var x_offset = parseInt(canvas_x / 40);
+    var y_offset = parseInt(canvas_y / 40) + 1;
+ 
+    var txtCoord = document.getElementById("txtCoord");
+    txtCoord.value = x_offset + " " + y_offset;
+ 
+    if ((canvas_x > (40 * x_offset)) &&
+        (canvas_x < (40 * x_offset) + 16) &&
+        (canvas_y > (40 * y_offset) - 16) &&
+        (canvas_y < (40 * y_offset))) {
+ 
+        if (insideIndex == -1) {
+            insideIndex = ((y_offset - 1) * rowLength + (x_offset - 1));
+ 
             var context = canvas.getContext("2d");
-            context.font = "bold 24pt Courier";
             context.fillStyle = "white";
-            context.fillText(c, element[1], element[2]);
+            context.fillRect(x_offset * 40, y_offset * 40 - 20, 20, 20);
 
-            context.font = "bold 24pt Courier";
-            context.fillStyle = "black";
-            context.fillText(c, element[1], element[2]);
+            context.font = highlightFont;
+            context.fillStyle = highlightColor;
+ 
+            console.log("Filling " + letters.charAt(insideIndex) + " at " + (x_offset * 40) + " " + (y_offset * 40));
+            context.fillText(letters.charAt(insideIndex), x_offset * 40, y_offset * 40);
         }
+    } else if (insideIndex != -1) {
+ 
+        x_offset = (parseInt(insideIndex % columnLength) + 1) * 40;
+        y_offset = (parseInt(insideIndex / columnLength) + 1) * 40;
+ 
+        // console.log("x_offset is " + x_offset + " and y_offset is " + y_offset);
+ 
+        var context = canvas.getContext("2d");
+        context.fillStyle = "white";
+        context.fillRect(x_offset, y_offset - 20, 20, 20);
 
-        resetList = new Array();
+        context.font = defaultFont;
+        context.fillStyle = colorIndices[insideIndex];
+ 
+        console.log("Filling " + letters.charAt(insideIndex) + " at " + x_offset + " " + y_offset);
+        context.fillText(letters.charAt(insideIndex), x_offset, y_offset);
+ 
+        insideIndex = -1;
     }
 }
-
+ 
 function doTouchStart(event) {
+
+
     console.log("Touch Start!");
 }
-
+ 
 function doTouchMove(event) {
     console.log("Touch Move!");
 }
-
+ 
 function doTouchEnd(event) {
     console.log("Touch End!");
 }
-
+ 
 function checkCanvasForConsistency() {
     columnLength = parseInt(mazeCount / rowLength);
     var remainder = mazeCount % rowLength;
-
+ 
     if (remainder > 0) {
         alert("The maze grid is inexact. Expected letter count: " + (rowLength * columnLength));
         return;
